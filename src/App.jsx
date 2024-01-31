301ac36bfad1a3687af2981985749432784e7dc9
Rodolph <rodolphcorp@gmail.com>
2024-01-31 18:20:28 +0100

    banner

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ const App = () => (
 	<div>
 		<ResponsiveAppBar />
 		<CardMui />
-		<CardMui />
-		<CardMui />
 	</div>
 );
 
--- src/Components/CardMui.tsx
+++ src/Components/CardMui.tsx
@@ -7,33 +7,129 @@ import {
 	CardActions,
 	Button,
 	CardMedia,
+	Grid,
 } from "@mui/material";
 
 export const CardMui = () => {
 	return (
-		<Box width="300px" marginTop="2em">
-			<Card>
-				<CardMedia
-					component="img"
-					height="140"
-					image="https://source.unsplash.com/random"
-					alt="unsplash"
-				/>
-				<CardContent>
-					<Typography gutterBottom variant="h5" component="div">
-						REACT
-					</Typography>
+		<Grid container>
+			<Grid item xs={2}>
+				<Box width="300px" height="180px" mt="2em">
+					<Card>
+						<CardMedia
+							component="img"
+							height="140"
+							image="https://source.unsplash.com/random"
+							alt="unsplash"
+						/>
+						<CardContent>
+							<Typography
+								gutterBottom
+								variant="h5"
+								component="div"
+							>
+								REACT
+							</Typography>
 
-					<Typography variant="body2" color="text.secondary">
-						LKLKLKZZZZZZZZZZZZ JJJRNRKJNRKNRKRKRNRKRN
-					</Typography>
-				</CardContent>
-				<CardActions>
-					<Button size="small">Share</Button>
-					<Button size="small">Learn</Button>
-				</CardActions>
-			</Card>
-		</Box>
+							<Typography variant="body2" color="text.secondary">
+								LKLKLKZZZZZZZZZZZZ JJJRNRKJNRKNRKRKRNRKRN
+							</Typography>
+						</CardContent>
+						<CardActions>
+							<Button size="small">Share</Button>
+							<Button size="small">Learn</Button>
+						</CardActions>
+					</Card>
+				</Box>
+			</Grid>
+			<Grid item xs={2}>
+				<Box width="300px" height="180px" mt="2em">
+					<Card>
+						<CardMedia
+							component="img"
+							height="140"
+							image="https://source.unsplash.com/random"
+							alt="unsplash"
+						/>
+						<CardContent>
+							<Typography
+								gutterBottom
+								variant="h5"
+								component="div"
+							>
+								REACT
+							</Typography>
+
+							<Typography variant="body2" color="text.secondary">
+								LKLKLKZZZZZZZZZZZZ JJJRNRKJNRKNRKRKRNRKRN
+							</Typography>
+						</CardContent>
+						<CardActions>
+							<Button size="small">Share</Button>
+							<Button size="small">Learn</Button>
+						</CardActions>
+					</Card>
+				</Box>
+			</Grid>
+			<Grid item xs={2}>
+				<Box width="300px" height="180px" mt="2em">
+					<Card>
+						<CardMedia
+							component="img"
+							height="140"
+							image="https://source.unsplash.com/random"
+							alt="unsplash"
+						/>
+						<CardContent>
+							<Typography
+								gutterBottom
+								variant="h5"
+								component="div"
+							>
+								REACT
+							</Typography>
+
+							<Typography variant="body2" color="text.secondary">
+								LKLKLKZZZZZZZZZZZZ JJJRNRKJNRKNRKRKRNRKRN
+							</Typography>
+						</CardContent>
+						<CardActions>
+							<Button size="small">Share</Button>
+							<Button size="small">Learn</Button>
+						</CardActions>
+					</Card>
+				</Box>
+			</Grid>
+			<Grid item xs={2}>
+				<Box width="300px" height="180px" mt="2em">
+					<Card>
+						<CardMedia
+							component="img"
+							height="140"
+							image="https://source.unsplash.com/random"
+							alt="unsplash"
+						/>
+						<CardContent>
+							<Typography
+								gutterBottom
+								variant="h5"
+								component="div"
+							>
+								REACT
+							</Typography>
+
+							<Typography variant="body2" color="text.secondary">
+								LKLKLKZZZZZZZZZZZZ JJJRNRKJNRKNRKRKRNRKRN
+							</Typography>
+						</CardContent>
+						<CardActions>
+							<Button size="small">Share</Button>
+							<Button size="small">Learn</Button>
+						</CardActions>
+					</Card>
+				</Box>
+			</Grid>
+		</Grid>
 	);
 };
 export default CardMui;
